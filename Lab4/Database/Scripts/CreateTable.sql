554d2d83ce7e673d29f82f59df1f99b2848f793d
xairaven <alexkovalevkpi@gmail.com>
2024-04-07 18:39:08 +0300

    (Lab 4) Feat: Data SQL scripts

---
+++ Lab4/Database/Scripts/CreateTable.sql
@@ -0,0 +1,12 @@
+﻿USE DPGI
+GO;
+
+CREATE TABLE dbo.Books
+(
+    isbn             VARCHAR(30),
+    title            VARCHAR(255),
+    authors          VARCHAR(255),
+    publisher        VARCHAR(255),
+    publication_year SMALLINT
+)
+GO;
