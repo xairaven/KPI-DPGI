391fd71b04c6a2ca9b24c9c0a1b013942eebf2c4
xairaven <alexkovalevkpi@gmail.com>
2024-04-23 23:23:27 +0300

    (Lab 5) Feat: Joined table tab

--- Lab5/Views/MainWindow.xaml.cs
+++ Lab5/Views/MainWindow.xaml.cs
@@ -12,9 +12,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+
         using var dbContext = new LibraryDbContext();
         BooksGrid.DataContext = dbContext.Books.ToList();
         PublishersGrid.DataContext = dbContext.Publishers.ToList();
+
+        InitializeJoinedTable(dbContext);
+    }
+
+    private void InitializeJoinedTable(LibraryDbContext dbContext)
+    {
+        var joined = dbContext.Books.Join(dbContext.Publishers,
+            x => x.PublisherCode,
+            y => y.Id,
+            (x, y) => new
+            {
+                x.Isbn,
+                x.Title,
+                x.Authors,
+                Publisher = y.Name,
+                x.PublicationYear
+            });
+
+        JoinedGrid.DataContext = joined.ToList();
     }
 }
