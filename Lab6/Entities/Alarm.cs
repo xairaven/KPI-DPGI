257777f01c605f1d3fd4c10d9762887731eecb7a
xairaven <alexkovalevkpi@gmail.com>
2024-04-25 21:12:48 +0300

    (Lab 6) Feat: Scaffolding done

---
+++ Lab6/Entities/Alarm.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab6.Entities;
+
+public partial class Alarm
+{
+    public byte[] Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public bool IsAlarmEnabled { get; set; }
+
+    public DateTime Datetime { get; set; }
+}
