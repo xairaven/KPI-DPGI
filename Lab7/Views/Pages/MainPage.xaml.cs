063672f3c51136a3205713c891c8f13a33e908d5
xairaven <alexkovalevkpi@gmail.com>
2024-05-05 22:20:44 +0300

    (Lab 7) Feat: Toolbar

--- Lab7/Views/Pages/MainPage.xaml.cs
+++ Lab7/Views/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Lab7.Views.Pages;
 
@@ -7,5 +8,35 @@ public partial class MainPage : Page
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void UndoButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CreateButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EditButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FindButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
