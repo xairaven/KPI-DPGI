0632b0f44d65da7cbd6977e44e7866d86aef19b9
xairaven <alexkovalevkpi@gmail.com>
2024-05-06 00:12:20 +0300

    (Lab 7) Feat: Button Hyperlinks

--- Lab7/Views/MainWindow.xaml.cs
+++ Lab7/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using Lab7.Views.Pages;
 
 namespace Lab7.Views;
 
@@ -7,8 +9,22 @@ namespace Lab7.Views;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly Dictionary<string, Page> _pages;
+    
     public MainWindow()
     {
         InitializeComponent();
+
+        _pages = new Dictionary<string, Page>();
+        CreatePages();
+        
+        MainFrame.Navigate(_pages["MainPage"]);
+    }
+
+    private void CreatePages()
+    {
+        _pages["MainPage"] = new MainPage(MainFrame, _pages);
+        _pages["BooksPage"] = new BooksPage();
+        _pages["PublishersPage"] = new PublishersPage();
     }
 }
--- Lab7/Views/Pages/MainPage.xaml.cs
+++ Lab7/Views/Pages/MainPage.xaml.cs
@@ -1,13 +1,20 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace Lab7.Views.Pages;
 
 public partial class MainPage : Page
 {
-    public MainPage()
+    private readonly Frame _frame;
+    private readonly Dictionary<string, Page> _pages;
+
+    public MainPage(Frame frame, Dictionary<string, Page> pages)
     {
         InitializeComponent();
+
+        _frame = frame;
+        _pages = pages;
     }
 
     private void UndoButton_OnClick(object sender, RoutedEventArgs e)
@@ -38,5 +45,15 @@ public partial class MainPage : Page
     private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
     {
         throw new NotImplementedException();
+    }
+
+    private void HyperlinkBooks_OnClick(object sender, RoutedEventArgs e)
+    {
+        _frame.Navigate(_pages["BooksPage"]);
+    }
+
+    private void HyperlinkPublishers_OnClick(object sender, RoutedEventArgs e)
+    {
+        _frame.Navigate(_pages["PublishersPage"]);
     }
 }
