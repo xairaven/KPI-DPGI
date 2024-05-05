888eaa202cd537936515d9d8e7abd5dc9899a870
xairaven <alexkovalevkpi@gmail.com>
2024-05-05 21:03:28 +0300

    (Lab 7) Init: Project

---
+++ Lab7/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Lab7.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
